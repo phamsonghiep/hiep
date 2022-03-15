8f8878e9377478aa278d366d7409b833702a6109
phamsonghiep <101326070+phamsonghiep@users.noreply.github.com>
2022-03-15 16:34:36 +0700

    Add files via upload

---
+++ bai3.js
@@ -0,0 +1,47 @@
+function myFunction1() {
+    var a = document.getElementById("text_1".value);
+    a = parseInt(a);
+    var b = document.getElementById("text_2".value);
+    b = parseInt(b);
+    var c = document.getElementById("select");
+    var value = c.options[c.selectedIndex].value;
+    value = parseInt(value);
+    var k;
+    switch (value){
+        case 1:{
+            k = (a+(b*2))/3;
+            k = parseFloat(k);
+            document.getElementById('result').value = "k";
+        }
+        case 2:{
+            k = ((a*2)+(b*3))/5;
+            k = parseFloat(k);
+            document.getElementById('result').value = k;
+        }
+        case 3:{
+            k = ((a*2)+(b*3))/7;
+            k = parseFloat(k);
+            document.getElementById('result').value = k;
+        }
+    }
+    if(k>=9){
+        document.getElementById("display").innerHTML = "Học lực giỏi";
+        document.getElementById('display').style.color = "red";
+    }
+    if(k>=7 && k<9){
+        document.getElementById("display").innerHTML = "Học lực khá";
+        document.getElementById('display').style.color = "blue";
+    }
+    if(k>=5 && k<7){
+        document.getElementById("display").innerHTML = "Học lực Trung bình";
+        document.getElementById('display').style.color = "yellow";
+    }
+    if(k>=0 && k <5){
+        document.getElementById("display").innerHTML = "Học lực Yếu";
+        document.getElementById('display').style.color = "gray";
+    }
+}
+function myFunction2() {
+    document.getElementById('text_1').value = "";
+    document.getElementById('text_2').value = "";
+}
